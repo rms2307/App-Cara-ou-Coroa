9e9239ebefe2dac5ad8d1d639772bf68e94536a5
Robson Silva <rms2307@outlook.com>
2020-08-04 08:54:13 -0300

    criação e layout da segunda activity

---
+++ app/src/main/java/com/rms2307/caraoucoroa/activity/MainActivity.java
@@ -0,0 +1,16 @@
+package com.rms2307.caraoucoroa.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.rms2307.caraoucoroa.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
--- app/src/main/java/com/rms2307/caraoucoroa/activity/ResultadoActivity.java
+++ app/src/main/java/com/rms2307/caraoucoroa/activity/ResultadoActivity.java
@@ -1,14 +1,16 @@
-package com.rms2307.caraoucoroa;
+package com.rms2307.caraoucoroa.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.rms2307.caraoucoroa.R;
+
+public class ResultadoActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_resultado);
     }
 }
